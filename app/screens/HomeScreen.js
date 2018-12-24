83b5178a03536ece0ef531705f56bb7ec0e6eabf
hongtuanquyen <hongtuanquyen@gmail.com>
2018-12-24 10:35:14 +0700

    Update

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,7 +1,37 @@
 import React, {Component} from 'react';
-import {AsyncStorage, Text, View, Button} from 'react-native';
+import {TextInput, FlatList, AsyncStorage, Text, View, Button} from 'react-native';
+import '@firebase/auth'
+import '@firebase/database';
+import {firebaseApp} from '../components/FirebaseConfig'
+
+const rootRef = firebaseApp.database().ref();
+const animalRef = rootRef.child('animals');
 
 class HomeScreen extends Component {
+    constructor() {
+      super();
+      this.state={
+        animalName: '',
+        animals: []
+      }
+      this.textAnimals = React.createRef();  
+    }
+
+    componentDidMount() {
+      animalRef.on('value', (childSnapshot)=>{
+        const animals = [];
+        childSnapshot.forEach((doc)=>{
+          animals.push({
+            key: doc.key,
+            animalName: doc.toJSON().animalName
+          })
+          this.setState({
+            animals: animals
+          });
+        })
+      })
+    }
+
     static navigationOptions = ({navigation}) => {
       return {
         title: "Welcome to my app"  
@@ -18,10 +48,26 @@ class HomeScreen extends Component {
       }
     }
   
+    insertToDatabase = () => {
+      if(this.state.animalName.trim() !== ''){
+        animalRef.push({
+          animalName: this.state.animalName
+        });
+        this.setState({animalName:''});
+        this.textAnimals.current.clear();
+      }
+    }
+
     render() {
       return (
         <View>
           <Text>Welcome to my app</Text>
+          <TextInput ref={this.textAnimals} onChangeText={(text)=>{this.setState({animalName: text});}} />
+          <Button title="+" onPress={()=>{this.insertToDatabase();}} />
+          <FlatList
+            data={this.state.animals}
+            renderItem={({item}) => <Text>{item.animalName}</Text>}
+          />
           <Button title="Logout" onPress={()=>{this.logout()}} />
         </View>
       );
